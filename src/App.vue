8054b5414e2fc59a4db156cad74abc9b21fbf994
ivozoee <1278875907@qq.com>
2021-12-06 18:03:01 +0800

    首页、详情页、购物车开发

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- Detail不需要缓存，每点击一个list都要重新请求一次数据 -->
+    <keep-alive exclude="Detail">
+      <router-view></router-view>
+    </keep-alive>
+    <main-tab-bar></main-tab-bar>
+  </div>
+</template>
+
+<script>
+import MainTabBar from "components/content/mainTabbar/mainTabBar.vue";
+export default {
+  name: "App",
+  components: {
+    MainTabBar,
+  },
+};
+</script>
+
+<style>
+@import url(assets/css/base.css);
+</style>
---
+++ src/views/cart/Cart.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="cart">
+    <!-- 购物车最上面 -->
+    <nav-bar class="nav-cart">
+      <template #center>
+        <div>购物车（{{ cartLength }}）</div>
+      </template>
+    </nav-bar>
+    <!-- 商品的列表 -->
+    <cart-list></cart-list>
+    <!-- 购物车底部的汇总 -->
+    <cart-bottom-bar></cart-bottom-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar.vue";
+
+import CartList from "./childComps/CartList.vue";
+import CartBottomBar from "./childComps/CartBottomBar.vue";
+
+import { mapGetters } from "vuex";
+export default {
+  name: "Cart",
+  components: {
+    NavBar,
+    CartList,
+    CartBottomBar,
+  },
+  computed: {
+    //方法一
+    // cartLength() {
+    //   return this.$store.getters.cartLength;
+    // },
+    //方法二
+    ...mapGetters(["cartLength"]),
+  },
+};
+</script>
+
+<style scoped>
+.cart {
+  height: 100vh;
+}
+.nav-cart {
+  background-color: var(--color-tint);
+  color: #fff;
+  font-weight: 700;
+  text-align: center;
+}
+</style>
